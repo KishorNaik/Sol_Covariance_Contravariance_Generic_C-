69456981446844e220059582a4857fd4480ba6d8
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-16 13:43:50 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sol_Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IDemo<UserModel, UserModel> demo = new Demo();
+            var userModel = demo.DemoMethod(new UserModel()
+            {
+                FullName = "Kishor Naik"
+            });
+            Console.WriteLine(userModel.FullName);
+        }
+    }
+
+    public interface IDemo<out TReturn, in TPara>
+        where TReturn : class
+        where TPara : class
+    {
+        TReturn DemoMethod(TPara para);
+    }
+
+    public class UserModel
+    {
+        public String FullName { get; set; }
+    }
+
+    public class Demo : IDemo<UserModel, UserModel>
+    {
+        UserModel IDemo<UserModel, UserModel>.DemoMethod(UserModel para)
+        {
+            return para;
+        }
+    }
+}
